8476b0123664b1f9d9726650c9dc46d441b97760
TauOmicronMu <tauomicronmurs@gmail.com>
2016-10-10 19:09:29 +0100

    Made a static image of pong and started storing game state

--- Main.hs
+++ Main.hs
@@ -10,17 +10,53 @@ offset = 100
 window :: Display
 window = InWindow "Haskell Pong" (width, height) (offset, offset)
 
-background, ballColor, paddleColor :: Color
+background, ballColor, paddleColor, wallColor, p1BorderColor, p2BorderColor :: Color
 background = black
 ballColor = light green
 paddleColor = light red
+wallColor = greyN 0.75
+p1BorderColor = rose
+p2BorderColor = yellow
 
-drawing :: Picture
-drawing = pictures [color ballColor   $ circleSolid 30, 
-                    color paddleColor $ rectangleSolid 10 50]
+-- | Holds the state of the current pong game
+data GameState = Game {
+    ballLoc :: (Float, Float), -- ^ (x, y) location of the ball
+    ballVel :: (Float, Float), -- ^ (x, y) velocity of the ball
+    player1 :: Float, -- ^ height of the 1st (left) player's paddle
+    player2 :: Float -- ^ height of the 2nd (right) player's paddle
+} deriving Show
+
+initialState :: GameState
+initialState = Game {
+    ballLoc = (-10, 30),
+    ballVel = (1, -3),
+    player1 = 40,
+    player2 = -80
+}
+
+-- | Convert a game state into a renderable picture
+render :: GameState -> Picture
+render game = pictures [ball,
+                        walls,
+                        genPaddle p1BorderColor 120    $ player1 game,
+                        genPaddle p2BorderColor (-120) $ player2 game]
+    where 
+        -- The pong ball
+        ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid 10
+  
+        -- The walls at the top and bottom of the screen
+        wall :: Float -> Picture
+        wall offset = translate 0 offset $ color wallColor $ rectangleSolid 270 10
+        
+        walls = pictures [wall (150), wall (-150)]
+
+        -- The paddles for each player
+        genPaddle :: Color -> Float -> Float -> Picture
+        genPaddle borderColor x y = pictures [translate x y $ color borderColor $ rectangleSolid 26 86,
+                                              translate x y $ color paddleColor $ rectangleSolid 20 80]
 
 main :: IO()
-main = display window background drawing
+main = display window background (render initialState)
 
 
 
