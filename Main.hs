6a0f36f258643fb9de12b05ae063af134eb591e7
TauOmicronMu <tauomicronmurs@gmail.com>
2016-10-10 18:04:52 +0100

    made a circle in a window :D

---
+++ Main.hs
@@ -0,0 +1,18 @@
+module Main(main) where
+
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "My First Window" (200, 200) (10, 10)
+
+background :: Color
+background = white
+
+drawing :: Picture
+drawing = circle 80
+
+main :: IO()
+main = display window background drawing
+
+
+
